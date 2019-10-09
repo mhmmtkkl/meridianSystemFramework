cef0f785daa93c6703c460f50cf3386c885a09fc
mhmmtkkl <makoklu32@gmail.com>
2019-10-08 22:06:46 -0400

    last changes

--- src/test/java/Runner/SmokeTestRunner.java
+++ src/test/java/Runner/SmokeTestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-
+  
 @RunWith(Cucumber.class)
 @CucumberOptions(strict = true,
 	plugin = {"pretty" , "html:target/cucumberHTMLRepo.html" , "json:target/cucumberJSON.json","rerun:target/rerun.txt" }	,
