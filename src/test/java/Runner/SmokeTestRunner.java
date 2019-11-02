0b3bd1c4275d1d9481fcf6c3ee7519194fec32b4
mhmmtkkl <makoklu32@gmail.com>
2019-11-01 21:58:52 -0400

    'jenkins integration'

--- src/test/java/Runner/SmokeTestRunner.java
+++ src/test/java/Runner/SmokeTestRunner.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
   
 @RunWith(Cucumber.class)
 @CucumberOptions(strict = true,
-	plugin = {"pretty" , "html:target/cucumberHTMLRepo.html" , "json:target/cucumberJSON.json","rerun:target/rerun.txt" }	,
+	plugin = {"pretty" , "html:target/cucumberHTMLRepo.html" , "json:target/cucumberJSON.json" }	,
 	tags="@SmokeTest2",
 	features = {"src/feature/test"},
 	glue = {"StepDefinition"}
